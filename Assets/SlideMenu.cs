d130221efd02dc9e11a4dc8e3346e0c9c75fe54f
sitozzz <sit7602@gmail.com>
2017-05-22 12:16:49 +0500

    Анимация меню умений
    Изменения в сцене и скрипт для управления

---
+++ Assets/SlideMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideMenu : MonoBehaviour {
+    public Animator slideMenu;
+	// Use this for initialization
+	void Start () {
+
+        slideMenu.SetBool("isHidden", true);
+		
+	}
+    public void arrowClick()
+    {
+        if (slideMenu.GetBool("isHidden") == false)
+        {
+            slideMenu.SetBool("isHidden", true);
+        }
+        else
+        {
+            slideMenu.SetBool("isHidden", false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
